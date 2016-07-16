330681bfbeca76fa11a836dcd2abe82482b0bcd3
Jonathan Paulson <jonathanpaulson@gmail.com>
2016-07-16 09:11:25 -0400

    Moving works.

--- client/src/main/scala/ClientMain.scala
+++ client/src/main/scala/ClientMain.scala
@@ -5,6 +5,7 @@ import org.scalajs.dom
 import org.scalajs.dom.html
 import scala.util.Random
 import scala.scalajs.js
+import scala.util.{Try, Success, Failure}
 
 case class Point(x: Double, y: Double){
   def +(p: Point) = Point(x + p.x, y + p.y)
@@ -20,6 +21,12 @@ object ClientMain extends JSApp {
   }
   def line(ctx : dom.CanvasRenderingContext2D, point : Point) : Unit = {
     ctx.lineTo(Math.floor(point.x), Math.floor(point.y));
+  }
+  def text(ctx : dom.CanvasRenderingContext2D, text : String, point : Point, color : String) : Unit = {
+    ctx.globalAlpha = 1.0
+    ctx.fillStyle = color
+    ctx.textAlign = "center"
+    ctx.fillText(text, Math.floor(point.x), Math.floor(point.y))
   }
 
   def hex_round(pos : Point) : Loc = {
@@ -87,8 +94,8 @@ object ClientMain extends JSApp {
 
     val board = BoardState.create(Plane.create(10, 10, HexTopology, Ground))
 
+    var selected : Option[Piece] = None
     var mouse = Loc(0, 0)
-    var selected = Loc(2, 0)
     var path = List(Loc(0, 0))
 
     // Update path to be a shortest path from [selected] to [mouse] that
@@ -97,25 +104,34 @@ object ClientMain extends JSApp {
       def distance(x : Loc, y : Loc) : Int = {
         board.tiles.topology.distance(x, y)
       }
-      if(path.size==0 || (!path(0).equals(selected))) {
-        path = List(selected)
-      }
-      while(path.size > 0 && distance(selected, mouse) != path.size-1 + distance(path(path.size-1), mouse)) {
-        path = path.init
-      }
-      if(path(path.size-1) != mouse) {
-        for(p <- board.tiles.topology.adj(path(path.size-1))) {
-          if(path.size-1 + distance(path(path.size-1), mouse) == path.size + distance(p, mouse)) {
-            path = path :+ p
+      selected match {
+        case None => ()
+        case Some(p) =>
+          val loc = p.loc
+          if(path.size==0 || (!path(0).equals(loc))) {
+            path = List(loc)
           }
-        }
+          while(path.size > 0 && distance(loc, mouse) != path.size-1 + distance(path(path.size-1), mouse)) {
+            path = path.init
+          }
+          if(path(path.size-1) != mouse) {
+            for(p <- board.tiles.topology.adj(path(path.size-1))) {
+              if(path.size-1 + distance(path(path.size-1), mouse) == path.size + distance(p, mouse)) {
+                path = path :+ p
+              }
+            }
+          }
       }
     }
 
     def show_board(board : BoardState) : Unit = {
       ctx.clearRect(0.0, 0.0, canvas.width.toDouble, canvas.height.toDouble)
       draw_hex(ctx, hex_center(mouse, origin), "purple", size-1.0)
-      draw_hex(ctx, hex_center(selected, origin), "red", size-1.0)
+      selected match {
+        case None => ()
+        case Some(p) =>
+          draw_hex(ctx, hex_center(p.loc, origin), "red", size-1.0)
+      }
       board.tiles.iteri {case ((x, y), tile) =>
         val center = hex_center(Loc(x,y), origin)
         tile.terrain match {
@@ -132,23 +148,36 @@ object ClientMain extends JSApp {
         }
       }
       board.pieces.iteri {case ((x,y), pieces) =>
+        def color(p : Piece) : String = {
+          selected match {
+            case None => "blue"
+            case Some(piece) =>
+              if (piece == p) "red" else "blue"
+          }
+        }
         val center = hex_center(Loc(x,y), origin)
         pieces match {
           case Nil => ()
           case p :: Nil =>
-            draw_hex(ctx, center, "blue", size-5.0)
+            draw_hex(ctx, center, color(p), size-5.0)
+            text(ctx, p.id.toString, center, "black")
           case p1 :: p2 :: Nil  =>
             val c1 = Point(center.x, center.y-size/2)
             val c2 = Point(center.x, center.y+size/2)
-            draw_hex(ctx, c1, "blue", size/2-0.5)
-            draw_hex(ctx, c2, "blue", size/2-0.5)
+            draw_hex(ctx, c1, color(p1), size/2-0.5)
+            text(ctx, p1.id.toString, c1, "black")
+            draw_hex(ctx, c2, color(p2), size/2-0.5)
+            text(ctx, p2.id.toString, c2, "black")
           case p1 :: p2 :: p3 :: Nil  => ()
             val c1 = Point(center.x, center.y-size/2)
             val c2 = (center+hex_corner(center, size, 2))/2
             val c3 = (center+hex_corner(center, size, 0))/2
-            draw_hex(ctx, c1, "blue", size/2-0.5)
-            draw_hex(ctx, c2, "blue", size/2-0.5)
-            draw_hex(ctx, c3, "blue", size/2-0.5)
+            draw_hex(ctx, c1, color(p1), size/2-0.5)
+            text(ctx, p1.id.toString, c1, "black")
+            draw_hex(ctx, c2, color(p2), size/2-0.5)
+            text(ctx, p2.id.toString, c2, "black")
+            draw_hex(ctx, c3, color(p3), size/2-0.5)
+            text(ctx, p3.id.toString, c3, "black")
           case _ => ()
         }
       }
@@ -166,22 +195,58 @@ object ClientMain extends JSApp {
       }
     }
 
-    def mouse_pos(e : dom.MouseEvent) : Loc = {
+    def mouse_to_hex(e : dom.MouseEvent) : Point = {
       val rect = canvas.getBoundingClientRect()
       val pixel_pos = Point(e.clientX - rect.left - origin.x, e.clientY - rect.top - origin.y)
-      val hex_pos_f = Point((pixel_pos.x * Math.sqrt(3)/3 - pixel_pos.y/3) / size, pixel_pos.y * 2.0/3.0 / size)
-      val hex_pos = hex_round(hex_pos_f)
-      hex_pos
+      Point((pixel_pos.x * Math.sqrt(3)/3 - pixel_pos.y/3) / size, pixel_pos.y * 2.0/3.0 / size)
+    }
+
+    def mouse_unit(e : dom.MouseEvent) : Option[Piece] = {
+      val hex_point = mouse_to_hex(e)
+      val hex_loc = hex_round(hex_point)
+      val hex_delta = Point(hex_point.x - hex_loc.x, hex_point.y - hex_loc.y)
+      println(hex_delta.x+" "+hex_delta.y)
+      board.pieces(hex_loc) match {
+        case Nil => None
+        case p :: Nil => Some(p)
+        case p1 :: p2 :: Nil =>
+          if(hex_delta.y < 0) {
+            Some(p1)
+          } else {
+            Some(p2)
+          }
+        case p1 :: p2 :: p3 :: Nil =>
+          if(hex_delta.y < 0) {
+            Some(p1)
+          } else if(hex_delta.x < 0) {
+            Some(p2)
+          } else {
+            Some(p3)
+          }
+        case _ => None
+      }
+    }
+
+    def mouse_pos(e : dom.MouseEvent) : Loc = {
+      hex_round(mouse_to_hex(e))
     }
 
     def mousedown(e : dom.MouseEvent) : Unit = {
-      if(board.pieces(selected).length == 1) {
-        val piece = board.pieces(selected)(0)
-        val action = Movements(List(Movement(StartedTurnWithID(piece.id), path.toArray)))
-        val result = board.doAction(action)
-        println(result)
+      selected match {
+        case None =>
+          selected = mouse_unit(e)
+        case Some(piece) =>
+          val action = Movements(List(Movement(StartedTurnWithID(piece.id), path.toArray)))
+          val result = board.doAction(action)
+          selected = None
+          path = List()
+          result match {
+            case Success(_) => ()
+            case Failure(error) =>
+              println(error)
+              selected = mouse_unit(e)
+          }
       }
-      selected = mouse_pos(e)
       show_board(board)
     }
     def mousemove(e : dom.MouseEvent) : Unit = {
