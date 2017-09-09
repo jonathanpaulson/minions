505739faa0b8216f50a9e40474b309491eacb363
David Wu <lightvector@gmail.com>
2017-09-09 15:09:26 -0400

    Implement basics in server

--- server/src/main/scala/ServerMain.scala
+++ server/src/main/scala/ServerMain.scala
@@ -1,17 +1,18 @@
 package minionsgame.server
 
 import scala.util.{Try,Success,Failure}
+import scala.concurrent.Future
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Flow,Sink,Source}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message,TextMessage}
 import akka.http.scaladsl.server.Directives._
 
 import minionsgame.core._
 
-object ServerMain {
+object ServerMain extends App {
   implicit val actorSystem = ActorSystem()
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = actorSystem.dispatcher
@@ -22,16 +23,71 @@ object ServerMain {
 
   val numBoards = config.getInt("app.numBoards")
 
-  val boards = {
+  val boards: Array[Board] = {
     val topology = HexTopology
-    val _ = Plane.create(12, 12, topology, Ground)
+    val boards = (0 until numBoards).toArray.map { _ =>
+      val plane: Plane[Terrain] = Plane.create(12, 12, topology, Ground)
+      plane(1,0) = Water
 
+      val state = BoardState.create(plane)
+      state.spawnPieceInitial(S0, Units.zombie.name, Loc(2,1))
+      state.spawnPieceInitial(S0, Units.zombie.name, Loc(2,2))
+      state.spawnPieceInitial(S0, Units.zombie.name, Loc(2,2))
+
+      state.spawnPieceInitial(S0, Units.zombie.name, Loc(2,3))
+      state.spawnPieceInitial(S0, Units.zombie.name, Loc(2,3))
+      state.spawnPieceInitial(S0, Units.zombie.name, Loc(2,3))
+
+      state.spawnPieceInitial(S1, Units.zombie.name, Loc(3,2))
+      state.spawnPieceInitial(S1, Units.zombie.name, Loc(3,4))
+      state.spawnPieceInitial(S1, Units.zombie.name, Loc(1,4))
+      state.spawnPieceInitial(S1, Units.zombie.name, Loc(2,5))
+
+      Board.create(state)
+    }
+    boards
+  }
+  val boardSequences: Array[Int] = (0 until numBoards).toArray.map { _ => 0}
+
+  def handleQuery(query: Protocol.Query): Protocol.Response = {
+    query match {
+      case Protocol.RequestGeneralState =>
+        Protocol.QueryError("Not implemented yet")
+      case Protocol.RequestBoardState(boardIdx) =>
+        if(boardIdx < 0 || boardIdx >= numBoards)
+          Protocol.QueryError("Invalid boardIdx")
+        else
+          Protocol.ReportBoardState(boardIdx,boards(boardIdx).curState(),boardSequences(boardIdx))
+      case Protocol.DoBoardAction(boardIdx,boardAction,boardSequence) =>
+        if(boardIdx < 0 || boardIdx >= numBoards)
+          Protocol.QueryError("Invalid boardIdx")
+        else {
+          val _ = (boardAction,boardSequence)
+          Protocol.QueryError("Not implemented yet")
+        }
+    }
   }
 
-
-  val echoService: Flow[Message, Message, _] = Flow[Message].map {
-    case TextMessage.Strict(txt) => TextMessage("ECHO: " + txt)
-    case _ => TextMessage("Message type unsupported")
+  val gameService: Flow[Message, Message, _] = {
+    import play.api.libs.json._
+    Flow[Message].collect {
+      case TextMessage.Strict(text)             => Future.successful(text)
+      case TextMessage.Streamed(textStream)     => textStream.runFold("")(_ + _)
+    } .mapAsync(1)(x => x)
+      .map { txt =>
+        val response: Protocol.Response =
+          Try(Json.parse(txt)) match {
+            case Failure(err) => Protocol.QueryError("Could not parse as json: " + err.toString)
+            case Success(json) =>
+              json.validate[Protocol.Query] match {
+                case (e: JsError) => Protocol.QueryError("Could not parse as query: " + JsError.toJson(e).toString())
+                case (s: JsSuccess[Protocol.Query]) =>
+                  val query = s.get
+                  handleQuery(query)
+              }
+          }
+        TextMessage(Json.stringify(Json.toJson(response)))
+      }
   }
 
   val route = get {
@@ -42,7 +98,7 @@ object ServerMain {
   path("playGame") {
     parameter("userName") { userName =>
       parameter("side") { side =>
-        handleWebSocketMessages(echoService)
+        handleWebSocketMessages(gameService)
       }
     }
   }
