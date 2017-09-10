5203b5f448c294a46d853fcec78a132fa2734747
David Wu <lightvector@gmail.com>
2017-09-10 19:22:05 -0400

    Connection class

--- client/src/main/scala/Connection.scala
+++ client/src/main/scala/Connection.scala
@@ -1,9 +1,15 @@
 package minionsgame.jsclient
 
+import scala.util.{Try,Success,Failure}
+import scala.concurrent.{Future,Promise}
+
 import org.scalajs.dom.WebSocket
 import org.scalajs.dom.Event
 import org.scalajs.dom.ErrorEvent
 import org.scalajs.dom.MessageEvent
+import java.net.URLEncoder
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import play.api.libs.json._
 
@@ -13,54 +19,38 @@ import RichImplicits._
 object Connection {
   def apply(
     baseUri: String,
-    gameHash: String,
     side: Side,
     username: String
   ): Connection = {
-    new Connection(baseUri,gameHash,side,username)
+    new Connection(baseUri,side,username)
   }
 }
 
 class Connection private (
   baseUri: String,
-  gameHash: String,
   side: Side,
   username: String
 ) {
 
-  //TODO proper url encoding
-  val uri = baseUri + "?" + "gameHash=" + gameHash + "side=" + side.int + "username=" + username
-  val socket = new WebSocket(uri)
+  def encode(s: String) = URLEncoder.encode(s, "UTF-8")
+  val uri = baseUri + "?" + "username=" + encode(username) + "side=" + side.int
 
-  socket.onopen = { (event: Event) =>
-  }
+  def run(f:Try[Protocol.Response] => Unit): Future[Unit] = {
+    val done: Promise[Unit] = Promise()
 
-  socket.onerror = { (event: ErrorEvent) =>
-  }
-
-  socket.onmessage = { (event: MessageEvent) =>
-    val message = Json.fromJson[Protocol.Message](Json.parse(event.data.toString))
-    message match {
-      case e:JsError => println("Error parsing message: " + e)
-      case s:JsSuccess[Protocol.Message] =>
-        s.get match {
-          case Protocol.Version(version) =>
-            println(version)
-          case Protocol.NumBoards(numBoards) =>
-            println(numBoards)
-          case Protocol.ReportBoardAction(boardIdx,boardAction,boardSequence) =>
-            println(boardIdx)
-            println(boardSequence)
-            println(boardAction)
-          case Protocol.ReportBoardState(boardIdx,boardState,boardSequence) =>
-            println(boardIdx)
-            println(boardSequence)
-            println(boardState)
-        }
+    val socket = new WebSocket(uri)
+    socket.onopen = { (_: Event) => () }
+    socket.onerror = { (event: ErrorEvent) => f(Failure(new Exception(event.message))) }
+    socket.onmessage = { (event: MessageEvent) =>
+      val message = Try(Json.fromJson[Protocol.Response](Json.parse(event.data.toString)))
+      message match {
+        case Failure(exn) => f(Failure(exn))
+        case Success(e:JsError) => f(Failure(new Exception("Error parsing message: " + e)))
+        case Success(s:JsSuccess[Protocol.Response]) => f(Success(s.get))
+      }
     }
-  }
+    socket.onclose = { (_: Event) => done.success(()) }
 
-  socket.onclose = { (event: Event) =>
+    done.future
   }
-
 }
