a1c539af802a125597393deb9da1f3447d50d663
Jonathan Paulson <jonathanpaulson@gmail.com>
2017-10-14 23:53:51 -0400

    Tweak graveyard border

--- client/src/main/scala/Drawing.scala
+++ client/src/main/scala/Drawing.scala
@@ -162,11 +162,10 @@ object Drawing {
         case Graveyard =>
           val texture = BoardMaps.groundImage(boardNames(boardIdx))
           fillHexWithTexture(hexLoc, texture, scale)
-          strokeHex(hexLoc, "#000000", scale, alpha=0.5, lineWidth=2.0)
+          strokeHex(hexLoc, "#000000", scale, alpha=0.5, lineWidth=2.5)
           val img = "img_terrain_graveyard" + deterministicRandom(loc.x,loc.y,4)
           fillHexWithImage(hexLoc, img, scale)
           //fillHex(hexLoc, "#aa8899", scale)
-          strokeHex(hexLoc, "#776677", scale, alpha=0.4, lineWidth=1.5)
         case SorceryNode =>
           val texture = BoardMaps.groundImage(boardNames(boardIdx))
           fillHexWithTexture(hexLoc, texture, scale)
