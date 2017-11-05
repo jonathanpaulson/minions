6ad7d24253b2d0814590fbf677465edfb0cc3730
Jonathan Paulson <jonathanpaulson@gmail.com>
2017-11-05 09:52:52 -0500

    Move EndTurn back (it was covering sorcery power)

--- client/src/main/scala/MiscUI.scala
+++ client/src/main/scala/MiscUI.scala
@@ -105,7 +105,7 @@ case class UI(val flipDisplay: Boolean, val ourSide: Option[Side], val boardXSiz
 
   //Positioning for end turn hex button
   object EndTurn extends UI.Component with UI.Clickable {
-    val origin = HexLoc(13.35,-1.7)
+    val origin = HexLoc(14.35,-1.7)
     val gridSizeScale = 1
 
     def getMouseTarget(game: Game, board: BoardState, hexLoc: HexLoc): MouseTarget = {
